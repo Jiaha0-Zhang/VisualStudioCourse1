a743998f5e28c2bede38c3d371cb295da464c408
Jiaha0-Zhang <xihd2017@gmail.com>
2023-12-29 20:35:59 -0800

    project upload

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+const progressBar = document.getElementsByClassName('progress-bar')[0]
+setInterval(() => {
+    const computedStyle = getComputedStyle(progressBar)
+    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+    progressBar.style.setProperty('--width',width + .1)
+},5)
